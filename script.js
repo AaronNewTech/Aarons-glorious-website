74b7e90464216093feca431f32f57f0cc20c65bd
Aaron <arnhsmith@gmail.com>
2023-06-01 10:02:25 -0400

    add css styled ship with movement

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+// Your code here
+
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
+const ship = document.getElementById("ship");
+
+function moveDodgerLeft() {
+    const leftNumbers = ship.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      ship.style.left = `${left - 12}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const leftNumbers = ship.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > -100 && left < 910) {
+      ship.style.left = `${left + 12}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
