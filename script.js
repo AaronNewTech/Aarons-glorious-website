12cc82b937d8a8ed2b87186f09c24a0a3cff417b
Aaron Smith <arnhsmith@gmail.com>
2025-06-22 18:26:30 -0400

    formating fixes

--- script.js
+++ script.js
@@ -1,86 +1,93 @@
 // Your code here
 let windowWidth = window.innerWidth;
 
-function delayAbout (URL) {
-    setTimeout( function() { window.location = URL }, 2000 );
+function delayAbout(URL) {
+  setTimeout(function () {
+    window.location = URL;
+  }, 2000);
 }
 
-document.getElementById("bee").onmousedown = function(){
-  const sound2 = document.createElement("audio");
-        sound2.src = ("https://github.com/AaronNewTech/Aarons-glorious-website/blob/main/audio/Galaga_Kill_Enemy_Sound_Effect2.mp3?raw=true")
-        function play() {
-          sound2.play() 
-        }
+document.getElementById('bee').onmousedown = function () {
+  const sound2 = document.createElement('audio');
+  sound2.src =
+    'https://github.com/AaronNewTech/Aarons-glorious-website/blob/main/audio/Galaga_Kill_Enemy_Sound_Effect2.mp3?raw=true';
+  function play() {
+    sound2.play();
+  }
   setTimeout(play, 500);
 };
 
-document.getElementById("boss").onmousedown = function(){
-  const sound2 = document.createElement("audio");
-        sound2.src = ("https://github.com/AaronNewTech/Aarons-glorious-website/blob/main/audio/Galaga_Kill_Enemy_Sound_Effect2.mp3?raw=true")
-        function play() {
-          sound2.play() 
-        }
+document.getElementById('boss').onmousedown = function () {
+  const sound2 = document.createElement('audio');
+  sound2.src =
+    'https://github.com/AaronNewTech/Aarons-glorious-website/blob/main/audio/Galaga_Kill_Enemy_Sound_Effect2.mp3?raw=true';
+  function play() {
+    sound2.play();
+  }
   setTimeout(play, 500);
 };
 
-document.getElementById("butterfly").onmousedown = function(){
-  const sound2 = document.createElement("audio");
-        sound2.src = ("https://github.com/AaronNewTech/Aarons-glorious-website/blob/main/audio/Galaga_Kill_Enemy_Sound_Effect2.mp3?raw=true")
-        function play() {
-          sound2.play() 
-        }
+document.getElementById('butterfly').onmousedown = function () {
+  const sound2 = document.createElement('audio');
+  sound2.src =
+    'https://github.com/AaronNewTech/Aarons-glorious-website/blob/main/audio/Galaga_Kill_Enemy_Sound_Effect2.mp3?raw=true';
+  function play() {
+    sound2.play();
+  }
   setTimeout(play, 500);
 };
 
-document.body.onkeyup = function(e){
-    if (e.keyCode == 32) {
-        const shoot1 = document.createElement("audio");
-        shoot1.src = ("https://github.com/AaronNewTech/Aarons-glorious-website/blob/main/audio/Galaga_Firing_Sound_Effect2.mp3?raw=true")
-        shoot1.play();
-    }
-} 
+document.body.onkeyup = function (e) {
+  if (e.keyCode == 32) {
+    const shoot1 = document.createElement('audio');
+    shoot1.src =
+      'https://github.com/AaronNewTech/Aarons-glorious-website/blob/main/audio/Galaga_Firing_Sound_Effect2.mp3?raw=true';
+    shoot1.play();
+  }
+};
 
-document.body.onmousedown = function(e){
-    if (e.onmousedown = true) {
-        const shoot2 = document.createElement("audio");
-        shoot2.src = ("https://github.com/AaronNewTech/Aarons-glorious-website/blob/main/audio/Galaga_Firing_Sound_Effect2.mp3?raw=true")
-        shoot2.play();
-    }
-} 
+document.body.onmousedown = function (e) {
+  if ((e.onmousedown = true)) {
+    const shoot2 = document.createElement('audio');
+    shoot2.src =
+      'https://github.com/AaronNewTech/Aarons-glorious-website/blob/main/audio/Galaga_Firing_Sound_Effect2.mp3?raw=true';
+    shoot2.play();
+  }
+};
 
-const ship = document.getElementById("ship");
+const ship = document.getElementById('ship');
 
-const onMouseMove = (e) =>{
+const onMouseMove = (e) => {
   ship.style.left = e.pageX + 'px';
-}
+};
 
 document.addEventListener('mousemove', onMouseMove);
 
 function moveShipLeft() {
-    const leftNumbers = ship.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      ship.style.left = `${left - 12}px`;
-    }
-  }
+  const leftNumbers = ship.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
 
-  function moveShipRight() {
-    const leftNumbers = ship.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > -100 && left < windowWidth - 90) {
-      ship.style.left = `${left + 12}px`;
-    }
+  if (left > 0) {
+    ship.style.left = `${left - 12}px`;
   }
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-      moveShipLeft();
-    }
-  });
+}
 
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowRight") {
-      moveShipRight();
-    }
-  });
+function moveShipRight() {
+  const leftNumbers = ship.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > -100 && left < windowWidth - 90) {
+    ship.style.left = `${left + 12}px`;
+  }
+}
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowLeft') {
+    moveShipLeft();
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowRight') {
+    moveShipRight();
+  }
+});
